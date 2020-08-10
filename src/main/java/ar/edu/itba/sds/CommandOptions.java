b6d714fe3518810d3233e1ae5027777b0af281b7
Gabriel <gsilvati@itba.edu.ar>
2020-08-10 11:44:22 -0300

    first commit

---
+++ src/main/java/ar/edu/itba/sds/CommandOptions.java
@@ -0,0 +1,85 @@
+package main.java.ar.edu.itba.sds;
+
+import java.util.ArrayList;
+
+public class CommandOptions
+{
+    private int MSize;
+    private double interactionRadius;
+    private boolean periodicContour = false;
+    private boolean bruteForce = false;
+    private String staticPFile;
+    private String dynamicPFile;
+	
+    protected ArrayList<String> arguments;
+    public CommandOptions(String[] args) {
+        parse(args);
+    }
+    
+    public void parse(String[] args) {
+        arguments = new ArrayList<String>();
+        for ( int i = 0; i < args.length; i++ ) {
+            arguments.add(args[i]);
+        }
+    }
+    
+    public int size() {
+        return arguments.size();
+    }
+    
+    public boolean hasOption(String option) {
+        boolean hasValue = false;
+        String str;
+        for ( int i = 0; i < arguments.size(); i++ ) {
+            str = (String)arguments.get(i);
+            if ( true == str.equalsIgnoreCase(option) ) {
+                hasValue = true;
+                break;
+            }
+        }
+        return hasValue;
+    }
+    
+    public String valueOf(String option) {
+        String value = null;
+        String str;
+        for ( int i = 0; i < arguments.size(); i++ ) {
+            str = (String)arguments.get(i);
+            if ( true == str.equalsIgnoreCase(option) ) {
+                value = (String)arguments.get(i+1);
+                break;
+            }
+        }
+        return value;
+    }
+    
+    public void parseOptions(){
+
+        try{
+
+            dynamicPFile = valueOf("df");
+            staticPFile = valueOf("sf");
+
+            if (hasOption("M")){
+                MSize = Integer.parseInt(valueOf("M"));
+            }
+            
+            if (hasOption("rc")){
+                interactionRadius = Double.parseDouble(valueOf("rc"));
+            }
+            
+            if (hasOption("pc")){
+                periodicContour = true;
+            }
+
+            if(hasOption("bf")){
+                bruteForce = true;
+            }
+
+
+        }catch (Exception e){
+            System.out.println("Command is not valid.");
+        }
+    }
+    
+}
---
+++ src/main/java/ar/edu/itba/sds/MainApp.java
@@ -0,0 +1,13 @@
+package main.java.ar.edu.itba.sds;
+
+public class MainApp {
+
+	public static void main(String[] args)
+	{
+		System.out.println("ssss");
+		
+//	    CommandOptions cmd = new CommandOptions(args);
+	    
+	   
+	}
+}
