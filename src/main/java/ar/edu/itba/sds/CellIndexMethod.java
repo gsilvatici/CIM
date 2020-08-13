d03bbe21b1103acfa40e231a4d24f446b1d47932
Gabriel <gsilvati@itba.edu.ar>
2020-08-13 08:41:43 -0300

    CIM working

--- src/main/java/ar/edu/itba/sds/CellIndexMethod.java
+++ src/main/java/ar/edu/itba/sds/CellIndexMethod.java
@@ -11,7 +11,6 @@ public class CellIndexMethod {
 	private double rc;
 	private boolean pc;
 	private List<Particle> particles;
-//	List<List<Particle>> heads; 
 	List<Particle>[][] heads; 
 	
 	@SuppressWarnings("unchecked")
@@ -29,25 +28,10 @@ public class CellIndexMethod {
                 heads[i][j] = new LinkedList<Particle>();
             }
         }
-
-//		heads = new ArrayList<List<Particle>>();
-//
-//        for (int i = 0; i < M*M ; i++){
-//        	heads.add(new ArrayList<Particle>());
-//        }
 		
 	}
 	
     public void Compute() {
-    	
-        /* builds heads lists */
-//    	List<Particle>[][] heads = new LinkedList[M][M]; 
-//    	
-//        for (int i = 0; i < M ; i++) {
-//            for (int j = 0; j < M ; j++) {
-//                heads[i][j] = new LinkedList<Particle>();
-//            }
-//        }
 
         for (Particle p : particles){
             int i = (int) (p.getY()/(L/M));
@@ -57,12 +41,6 @@ public class CellIndexMethod {
             p.setXIndex(j);
             currentHead.add(p);
 
-            
-//            int cellNumber = (int) (cellY * M + cellX);
-//            List <Particle> cellParticles = heads.get(cellNumber);
-//            p.setCellX(cellX);
-//            p.setCellY(cellY);
-//            cellParticles.add(p);
         }
         
         for (int i = 0; i < M ; i++) {
@@ -71,12 +49,6 @@ public class CellIndexMethod {
 	                int xCell = p.getXIndex();
 	                int yCell = p.getYIndex();
 	                
-//	                checkNeighbourCell(p, heads[xCell][yCell], xCell, yCell);
-//	                checkNeighbourCell(p, heads[xCell][yCell], xCell + 1, yCell);
-//	                checkNeighbourCell(p, heads[xCell][yCell], xCell + 1, yCell + 1);
-//	                checkNeighbourCell(p, heads[xCell][yCell], xCell, yCell + 1);
-//	                checkNeighbourCell(p, heads[xCell][yCell], xCell + 1, yCell - 1);
-
 	                checkNeighbourCell(p, heads[xCell][yCell], xCell, yCell);
 	                checkNeighbourCell(p, heads[xCell][yCell], xCell + 1, yCell);
 	                checkNeighbourCell(p, heads[xCell][yCell], xCell + 1, yCell + 1);
@@ -110,7 +82,6 @@ public class CellIndexMethod {
             }
         }        
 
-//        for (Particle neighbourCellParticle : neighbours) {
         for (Particle neighbourCellParticle : this.heads[xCell][yCell]) {
 
             if (!neighbourCellParticle.equals(particle)) {
